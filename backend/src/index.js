97f09ea7fc7b805fe4c50170099f9a9122212e17
Gunanka D <gunanka2594@gmail.com>
2025-05-04 01:04:50 +0530

    implemented pages and components

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
 
+import cors from "cors"
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
@@ -17,6 +19,12 @@ app.use(express.json());
 
 // MIDDLEWARE: allows us to parse the cookies to grab values out of it
 app.use(cookieParser());
+
+// MIDDLWARE: this is used to allow cross-origin requests. required for the frontend to be able to make requests to the backend
+app.use(cors({
+    origin: "localhost:5173", // allow requests from this origin
+    credentials: true, // allow cookies to be sent with requests
+}));
 
 // ROUTE: route handler for authentication
 app.use("/api/auth", authRoutes)
